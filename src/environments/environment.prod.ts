82581daef8b270c9c8c6178cd4f89979c8a79105
kesien <balazsnobik@gmail.com>
2022-03-18 20:24:07 +0100

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseApiUrl: 'https://workschedule-flying.herokuapp.com/api/',
+  signalRUrl: 'https://workschedule-flying.herokuapp.com/schedule'
 };
