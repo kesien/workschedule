b92e990c2ed310a3f5e1ec0eef77909983311c93
kesien <66409501+kesien@users.noreply.github.com>
2022-05-31 22:29:27 +0200

    Feature/mobile styling (#7)
    * empty spaces removed from mobile view
    
    * whatsnew updated

--- src/app/components/schedule/schedule-row/schedule-row.component.ts
+++ src/app/components/schedule/schedule-row/schedule-row.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Day } from 'src/app/shared/models/day.model';
-import { ScheduleDay } from 'src/app/shared/models/scheduleday.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Day } from 'src/app/shared/models/day.model'
+import { ScheduleDay } from 'src/app/shared/models/scheduleday.model'
 
 @Component({
   selector: 'app-schedule-row',
@@ -8,26 +8,26 @@ import { ScheduleDay } from 'src/app/shared/models/scheduleday.model';
   styleUrls: ['./schedule-row.component.css'],
 })
 export class ScheduleRowComponent implements OnInit {
-  @Input() days!: ScheduleDay[];
-  @Input() editMode = false;
-  @Input() mobile = false;
-  @Output() arrowClick = new EventEmitter();
+  @Input() days!: ScheduleDay[]
+  @Input() editMode = false
+  @Input() mobile = false
+  @Output() arrowClick = new EventEmitter()
   constructor() {}
 
   ngOnInit(): void {}
 
   arrowClicked(day: Day) {
-    this.arrowClick.emit(day);
+    this.arrowClick.emit(day)
   }
 
-  shouldDisplay(day: Day | null) {    
+  shouldDisplay(day: Day | null) {
     if (this.mobile) {
-      if (day == null || day.isWeekend) {
-        return false;
+      if (day == null || (day.isWeekend && !day.isHoliday)) {
+        return false
       } else {
-        return true;
+        return true
       }
     }
-    return true;
+    return true
   }
 }
